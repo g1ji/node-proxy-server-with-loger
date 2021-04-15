ffb86b0513165f3f7bfc3ce46276de84b7c1ff02
g1ji <emails4me.gk@gmail.com>
2021-04-15 06:49:32 +0530

    v1

---
+++ app.js
@@ -0,0 +1,99 @@
+const crypto = require('crypto');
+const Express = require('express');
+const UniqId = require('uniqid');
+const app = Express();
+const BodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const proxy = require('express-http-proxy');
+
+const noSchema = new Schema({}, { strict: false });
+let Log = mongoose.model('logs', noSchema);
+
+let env = require('./env');
+
+let decrypt = function (encryptedData, encryptionMethod, secret, iv) {
+    encryptedData = Buffer.from(encryptedData, 'base64').toString();
+    let decipher = crypto.createDecipheriv(encryptionMethod, secret, iv);
+    return decipher.update(encryptedData, 'base64', 'utf8') + decipher.final('utf8');
+};
+
+let proxyReqPathResolver = function (req) {
+    let uniqid = UniqId();
+    let date = new Date();
+    let encryptedData = req.params.webhookId;
+    let encryptionMethod = env.encryptionMethod;
+    let secret = env.webhookSecret;
+    let iv = secret.substr(0, 16);
+    let userInfo = decrypt(encryptedData, encryptionMethod, secret, iv);
+    userInfo = JSON.parse(userInfo);
+    let logData = {
+        type: req.type,
+        identifier: uniqid,
+        request: req.body,
+        userInfo: userInfo,
+        webhookId: req.params.webhookId,
+        inTime: date.getTime(),
+        timeStr: date.toString(),
+        time: date
+    }
+    req.logData = logData;
+    let log = new Log(logData)
+    log.save().catch((e) => {
+        console.log(e)
+    })
+    return req.url;
+}
+let userResDecorator = function (proxyRes, proxyResData, userReq, userRes) {
+    let logData = {};
+    let date = new Date();
+
+    logData.response = JSON.parse(proxyResData.toString('utf8'));
+    logData.timeTaken = date.getTime() - userReq.logData.inTime;
+    Log
+        .findOneAndUpdate({
+            identifier: userReq.logData.identifier
+        }, {
+            $set: logData
+        }, {
+            upsert: true
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+    return proxyResData;
+}
+
+app.use(BodyParser.json({ limit: '50kb' }));
+app.use(BodyParser.urlencoded({ limit: '50kb', extended: true }));
+
+mongoose
+    .connect(env.db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.post('/series/:webhookId',
+            proxy(env.proxyTo, {
+                timeout: 2000,
+                https: false,
+                proxyReqPathResolver: function (req) {
+                    req.type = 'series';
+                    return proxyReqPathResolver(req);
+                },
+                userResDecorator: userResDecorator
+            })
+        )
+
+        app.post('/:webhookId',
+            proxy(env.proxyTo, {
+                timeout: 2000,
+                https: false,
+                proxyReqPathResolver: function (req) {
+                    req.type = 'parallel';
+                    return proxyReqPathResolver(req);
+                },
+                userResDecorator: userResDecorator
+            })
+        )
+    })
+var server = app.listen(env.port, function () {
+    console.log(`==> ${new Date()} Server is running at http://localhost:${env.port}/`);
+});
